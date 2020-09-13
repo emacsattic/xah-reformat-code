190c2b2ce7e3bb475661204beb1bd5ff083c7bd0
Xah Lee <xah@xahlee.org>
2020-09-13 10:01:20 -0700

    xah-reformat-lines now always make long line first. so, can be used in key macro.

--- xah-reformat-code.el
+++ xah-reformat-code.el
@@ -1,9 +1,9 @@
-;;; xah-reformat-code.el --- commands to reformat source code.
+;;; xah-reformat-code.el --- commands to reformat source code. -*- coding: utf-8; lexical-binding: t; -*-
 
-;; Copyright © 2013-2017, by Xah Lee
+;; Copyright © 2013-2020, by Xah Lee
 
 ;; Author: Xah Lee ( http://xahlee.info/ )
-;; Version: 1.1.20170820
+;; Version: 1.1.20200913095901
 ;; Created: 13 Dec 2016
 ;; Keywords: convenience
 ;; License: GPL v3
@@ -60,53 +60,49 @@
 ;;; Code:
 
 ;;;###autoload
-(defun xah-reformat-lines ( &optional *length)
+(defun xah-reformat-lines ( &optional @length)
   "Reformat current text block into 1 long line or multiple short lines.
 When there is a text selection, act on the selection, else, act on a text block separated by blank lines.
 
-When the command is called for the first time, it checks the current line's length to decide to go into 1 line or multiple lines. If current line is short, it'll reformat to 1 long lines. And vice versa.
-
-Repeated call toggles between formatting to 1 long line and multiple lines.
+When the command is called for the first time, make it a long line. Second call makes it short line. Repeated call toggles.
 
 If `universal-argument' is called first, use the number value for min length of line. By default, it's 70.
 
 URL `http://ergoemacs.org/emacs/emacs_reformat_lines.html'
-Version 2017-07-06"
+Version 2020-09-12"
   (interactive)
   ;; This command symbol has a property “'is-longline-p”, the possible values are t and nil. This property is used to easily determine whether to compact or uncompact, when this command is called again
   (let* (
-         (*length (if *length
-                      *length
-                    (if current-prefix-arg (prefix-numeric-value current-prefix-arg) 70 )))
+         (@length (if @length
+                      @length
+                    (if current-prefix-arg (prefix-numeric-value current-prefix-arg) fill-column )))
          (is-longline-p
           (if (eq last-command this-command)
               (get this-command 'is-longline-p)
-            (> (- (line-end-position) (line-beginning-position)) *length)))
-         (deactivate-mark nil)
-         (-blanks-regex "\n[ \t]*\n")
-         -p1 -p2
+            nil))
+         ($blanks-regex "\n[ \t]*\n")
+         $p1 $p2
          )
     (if (use-region-p)
-        (progn (setq -p1 (region-beginning))
-               (setq -p2 (region-end)))
+         (setq $p1 (region-beginning) $p2 (region-end))
       (save-excursion
-        (if (re-search-backward -blanks-regex nil "move")
-            (progn (re-search-forward -blanks-regex)
-                   (setq -p1 (point)))
-          (setq -p1 (point)))
-        (if (re-search-forward -blanks-regex nil "move")
-            (progn (re-search-backward -blanks-regex)
-                   (setq -p2 (point)))
-          (setq -p2 (point)))))
-    (save-excursion
+        (if (re-search-backward $blanks-regex nil "move")
+            (progn (re-search-forward $blanks-regex)
+                   (setq $p1 (point)))
+          (setq $p1 (point)))
+        (if (re-search-forward $blanks-regex nil "move")
+            (progn (re-search-backward $blanks-regex)
+                   (setq $p2 (point)))
+          (setq $p2 (point)))))
+    (progn
       (if current-prefix-arg
-          (xah-reformat-to-multi-lines -p1 -p2 *length)
+          (xah-reformat-to-multi-lines $p1 $p2 @length)
         (if is-longline-p
-            (xah-reformat-to-multi-lines -p1 -p2 *length)
-          (xah-reformat-whitespaces-to-one-space -p1 -p2)))
+            (xah-reformat-to-multi-lines $p1 $p2 @length)
+          (xah-reformat-whitespaces-to-one-space $p1 $p2)))
       (put this-command 'is-longline-p (not is-longline-p)))))
 
-(defun xah-reformat-whitespaces-to-one-space (*begin *end)
+(defun xah-reformat-whitespaces-to-one-space (@begin @end)
   "Replace whitespaces by one space.
 
 URL `http://ergoemacs.org/emacs/emacs_reformat_lines.html'
@@ -114,7 +110,7 @@ Version 2017-01-11"
   (interactive "r")
   (save-excursion
     (save-restriction
-      (narrow-to-region *begin *end)
+      (narrow-to-region @begin @end)
       (goto-char (point-min))
       (while
           (search-forward "\n" nil "move")
@@ -128,46 +124,43 @@ Version 2017-01-11"
           (re-search-forward "  +" nil "move")
         (replace-match " ")))))
 
-(defun xah-reformat-to-multi-lines ( &optional *begin *end *min-length)
+(defun xah-reformat-to-multi-lines ( &optional @begin @end @min-length)
   "Replace spaces by a newline at places so lines are not long.
 When there is a text selection, act on the selection, else, act on a text block separated by blank lines.
 
 If `universal-argument' is called first, use the number value for min length of line. By default, it's 70.
 
 URL `http://ergoemacs.org/emacs/emacs_reformat_lines.html'
-Version 2017-07-06"
-  (interactive )
+Version 2018-12-16 2020-09-08"
+  (interactive)
   (let (
-        -p1 -p2
-        (-blanks-regex "\n[ \t]*\n")
-        (-minlen (if *min-length
-                     *min-length
-                   (if current-prefix-arg (prefix-numeric-value current-prefix-arg) 70 ))))
-    (if (and  *begin *end)
-        (setq -p1 *begin -p2 *end)
-      (if (region-active-p)
-          (progn (setq -p1 (region-beginning) -p2 (region-end)))
+        $p1 $p2
+        ($blanks-regex "\n[ \t]*\n")
+        ($minlen (if @min-length
+                     @min-length
+                   (if current-prefix-arg (prefix-numeric-value current-prefix-arg) fill-column))))
+    (if (and  @begin @end)
+        (setq $p1 @begin $p2 @end)
+      (if (use-region-p)
+          (progn (setq $p1 (region-beginning) $p2 (region-end)))
         (save-excursion
-          (if (re-search-backward -blanks-regex nil "move")
-              (progn (re-search-forward -blanks-regex)
-                     (setq -p1 (point)))
-            (setq -p1 (point)))
-          (if (re-search-forward -blanks-regex nil "move")
-              (progn (re-search-backward -blanks-regex)
-                     (setq -p2 (point)))
-            (setq -p2 (point))))))
-    (save-restriction
-      (narrow-to-region -p1 -p2)
-      (goto-char (point-min))
-      (while
-          (re-search-forward " +" nil "move")
-        (when (> (- (point) (line-beginning-position)) -minlen)
-          (replace-match "\n" ))))))
+          (if (re-search-backward $blanks-regex nil "move")
+              (progn (re-search-forward $blanks-regex)
+                     (setq $p1 (point)))
+            (setq $p1 (point)))
+          (if (re-search-forward $blanks-regex nil "move")
+              (progn (re-search-backward $blanks-regex)
+                     (setq $p2 (point)))
+            (setq $p2 (point))))))
+    (save-excursion
+      (save-restriction
+        (narrow-to-region $p1 $p2)
+        (goto-char (point-min))
+        (while
+            (re-search-forward " +" nil "move")
+          (when (> (- (point) (line-beginning-position)) $minlen)
+            (replace-match "\n" )))))))
 
 (provide 'xah-reformat-code)
-
-;; Local Variables:
-;; coding: utf-8
-;; End:
 
 ;;; xah-reformat-code.el ends here
